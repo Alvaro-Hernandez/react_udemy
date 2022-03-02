3653dbfd2d7726dbb262543b131a52ff2b2574e1
Alvaro-Hernandez <alvaropineda606@gmail.com>
2022-03-02 15:40:51 -0600

    Primeros Pasos en React

---
+++ 02_intro_React/src/bases/01_const_let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = "Alvaro";
+const apellido = "Hernandez";
+
+console.log(nombre, apellido);
+
+let valorDado = 5;
+valorDado = 4;
+
+if(true){
+    let valorDado = 5;
+    const nombre = "Petter";
+    console.log(valorDado, nombre);
+}
+
+console.log(valorDado);
---
+++ 02_intro_React/src/bases/02_template_string.js
@@ -0,0 +1,16 @@
+//Template String
+
+
+const nombre = "Alvaro";
+const apellido = " Hernandez";
+
+// const nombreCompleto = nombre+apellido;
+const nombreCompleto = ` ${nombre} ${apellido} `;
+
+console.log( nombreCompleto)
+
+function getSaludo (nombre) {
+    return "Hola " + nombre;
+}
+
+console.log( `Este es un Texto: ${ getSaludo(nombreCompleto) }`)
